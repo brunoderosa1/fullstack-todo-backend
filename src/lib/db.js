7956566b47a123d4fd3048e377658ac41f8b03f4
Bruno De Rosa <brunoderosa180@gmail.com>
2024-02-24 03:31:02 -0300

    add db functionality

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,4 +3,64 @@ import { PrismaClient } from "@prisma/client";
 if (!global.prisma) {
     global.prisma = new PrismaClient();
 }
-export default global.prisma;
+export const prisma = global.prisma;
+
+export const getTodos = async (userId) => {
+    return prisma.todo.findMany({
+        where: {
+            userId,
+        },
+        include: {  
+            userId: true,
+        }
+    });
+};
+
+export const getTodo = async (userId, id) => {
+    return prisma.todo.findUnique({
+        where: {
+            userId,
+            id
+        },
+        include: {
+            userId: true,
+        }
+    });
+};
+
+export const createTodo = async (userId, data) => {
+    return prisma.todo.create({
+        data: {
+            ...data,
+            userId
+        },
+        include: {
+            userId: true,
+        }
+    });
+};
+
+export const updateTodo = async (userId, id, data) => {
+    return prisma.todo.update({
+        where: {
+            userId,
+            id
+        },
+        data,
+        include: {
+            userId: true,
+        }
+    });
+};
+
+export const deleteTodo = async (userId, id) => {
+    return prisma.todo.delete({
+        where: {
+            userId,
+            id
+        },
+        include: {
+            userId: true,
+        }
+    });
+};
